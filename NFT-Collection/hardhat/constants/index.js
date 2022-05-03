b26c8a365ebc97292e16649f00b24a045b62a2f1
durmusgulbahar <durmus.gulbahar@outlook.com>
2022-05-03 17:58:11 +0300

    col

---
+++ NFT-Collection/hardhat/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x5B225f14bc9B526c997d1a9D9E157dA74D6e2a32";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
